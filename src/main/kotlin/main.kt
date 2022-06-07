00371f695438eae7f693d695ccb784febe8af82a
Maria Kuznetsova <mikuznetsova@list.ru>
2022-06-07 18:53:19 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Введите количество лайков:")
+    val likes = readln().toInt()
+    if (likes % 10 == 1 && likes % 100 != 11) {
+        println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
+    }
+}
